92f3edf6c293d1a1237e81c5abf9af94cbdc29be
1pali <pavol.cina@gmail.com>
2019-03-01 11:29:20 +0100

    heroku mongodb setup

--- server.js
+++ server.js
@@ -5,16 +5,26 @@ var express = require('express'),
   Predmet = require('./api/models/predmetyModel'), //created model loading here
   Kategoria = require('./api/models/kategorieModel'),
   Latka = require('./api/models/latkyModel'),
-  bodyParser = require('body-parser');
-  
+  bodyParser = require('body-parser'),
+  mongodb = require("mongodb"),
+  ObjectID = mongodb.ObjectID
 // mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongolab-curly-19764'); 
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongolab-curly-19764'); 
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongolab-curly-19764", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log("Database connection ready");
+});
 
 var routes = require('./api/routes/allapRoutes'); //importing route
 routes(app); //register the route
